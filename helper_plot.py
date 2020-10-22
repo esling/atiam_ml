c87a6754be95248cb8ecaf5bce00dad4cecbd3b0
Philippe Esling <esling@PRO-2903-esling.local>
2020-10-22 11:02:24 +0200

    Final version of ML notebook

--- helper_plot.py
+++ helper_plot.py
@@ -3,6 +3,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 def hdr_plot_style():
+    plt.style.use('dark_background')
     mpl.rcParams.update({'font.size': 18, 'lines.linewidth': 3, 'lines.markersize': 15})
     # avoid type 3 (i.e. bitmap) fonts in figures
     mpl.rcParams['ps.useafm'] = True
@@ -13,8 +14,9 @@ def hdr_plot_style():
     mpl.rcParams['text.hinting'] = False
     # Set colors cycle
     colors = mpl.cycler('color', ['#3388BB', '#EE6666', '#9988DD', '#EECC55', '#88BB44', '#FFBBBB'])
-    plt.rc('axes', facecolor='#FFFFFF88', edgecolor='white', axisbelow=True, grid=True, prop_cycle=colors)
-    plt.rc('legend', facecolor='#666666BB', edgecolor='white', fontsize=16)
+    #plt.rc('figure', facecolor='#00000000', edgecolor='black')
+    #plt.rc('axes', facecolor='#FFFFFF88', edgecolor='white', axisbelow=True, grid=True, prop_cycle=colors)
+    plt.rc('legend', facecolor='#666666EE', edgecolor='white', fontsize=16)
     plt.rc('grid', color='white', linestyle='solid')
     plt.rc('text', color='white')
     plt.rc('xtick', direction='out', color='white')
